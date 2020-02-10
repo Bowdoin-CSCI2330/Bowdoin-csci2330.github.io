093777db80512c0f41f51704b3f6fb16ad3b39bd
Stephen Houser <stephenhouser@gmail.com>
2020-02-10 11:09:32 -0500

    add example for memory

---
+++ _samples/zips1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ZIP_LENGTH 5
+
+int main(int argc, char **argv) {
+	int* zip = (int*) malloc(sizeof(int) * ZIP_LENGTH);
+	if (zip == NULL) {           
+		perror("malloc failed");
+		exit(0);                
+	}
+
+	zip[0] = 0;
+	zip[1] = 4;
+	zip[2] = 0;
+	zip[3] = 1;
+	zip[4] = 1;
+
+	printf("zip is");
+	for (int i = 0; i < ZIP_LENGTH; i++) {
+		printf(" %d", zip[i]);
+	}
+	printf("\n");
+
+	free(zip);
+}
