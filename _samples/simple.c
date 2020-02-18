33f197b776f7c2ed6ffa34425f476f0528776397
Stephen Houser <stephenhouser@gmail.com>
2020-02-18 07:33:11 -0500

    add simple asm example

---
+++ _samples/simple.c
@@ -0,0 +1,15 @@
+// Type your code here, or load an example.
+#include <stdio.h>
+// int add(x, y ) {
+//     return x + y;
+// }
+
+int main() {
+    int x, y, z;
+    x = 42;
+    y = -5;
+    z = x + y;
+    // z = add(x, y)
+    printf("%d + %d = %d\n", x, y, z);
+    return z;
+}
